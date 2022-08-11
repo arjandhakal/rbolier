2ad78caf7748d81ab00d02db98dc5cb68b5f9079
Arjan Dhakal <dhakal.arjan@gmail.com>
2022-08-11 11:26:23 +0545

    added recommended hooks rule to the eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,7 @@ module.exports = {
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:import/typescript',
+    'plugin:react-hooks/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
