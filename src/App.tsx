4758246f7b7706a78698b5243884d630c8263aaf
Arjan Dhakal <dhakal.arjan@gmail.com>
2022-08-11 13:38:41 +0545

    some husky and linting changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import { useState } from 'react';
 function App() {
   const [count, setCount] = useState(0);
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen m-auto">
       <div className="m-auto">
         <div className="text-6xl text-red-600">{count}</div>
         <button
-          className="px-6 py-2 rounded bg-green-800 hover:bg-green-600 text-white"
           type="button"
+          className='px-6 py-2 rounded bg-green-800 hover:bg-green-600 text-white"
+          type="button'
           onClick={() => setCount((value) => value + 1)}
         >
           count+
